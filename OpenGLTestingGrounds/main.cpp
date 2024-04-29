8c8699ffcabd78e47648ef2f99d32a3af2e60921
FRLor <franzrey.lor@mymail.champlain.edu>
2024-04-28 23:42:42 -0600

    Updated: main -> triforce triangle using indices

--- OpenGLTestingGrounds/main.cpp
+++ OpenGLTestingGrounds/main.cpp
@@ -15,7 +15,7 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"   FragColor = vec4(0.3f, 0.5f, 0.9f, 1.0f);\n"
 "}\n\0";
 
 
@@ -37,6 +37,19 @@ int main() {
 		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
 		//upper right corner
 		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+		//inner left
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+		//inner right
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+		//inner down
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+	};
+
+	//lower left triangle, lower right, upper
+	GLuint indices[] = {
+		0, 3, 5,
+		3, 2, 4,
+		5, 4, 1
 	};
 
 	//main window, 800x800
@@ -77,11 +90,12 @@ int main() {
 
 
 	//ref contianers for vertex array obj & buffer
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
 	//generate vao before vbo
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
@@ -89,12 +103,17 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	//config vertex attr. for openGL to read VBO
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
 	//bind VAO & VBO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glfwSwapBuffers(window);
 
@@ -108,7 +127,7 @@ int main() {
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
@@ -118,6 +137,7 @@ int main() {
 	//destroy vertex
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	//deinit
